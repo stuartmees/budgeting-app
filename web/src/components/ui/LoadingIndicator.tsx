5910c3cc9148095602ea67ce4636aac990ff812b
Stuart Mees <35113861+stuartmees@users.noreply.github.com>
2026-03-11 23:15:01 +0000

    Adds type definition to LoadingIndicator text prop

--- web/src/components/ui/LoadingIndicator.tsx
+++ web/src/components/ui/LoadingIndicator.tsx
@@ -1,6 +1,6 @@
-const LoadingIndicator = () => (
+const LoadingIndicator = ({ text = 'Loading...' }: { text?: string }) => (
   <div className="flex-1 flex min-h-screen items-center justify-center">
-    <h2 className="text-3xl font-bold font-heading">Loading...</h2>
+    <h2 className="text-3xl font-bold font-heading">{text}</h2>
   </div>
 );
 
