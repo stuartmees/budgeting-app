1a10e9b55a808478da688d967249881ad0df24a6
Stuart Mees <35113861+stuartmees@users.noreply.github.com>
2026-03-11 22:04:44 +0000

    Adds API base URL from environment variable

--- web/src/constants/api.ts
+++ web/src/constants/api.ts
@@ -1,7 +1,9 @@
+const API_BASE_URL = import.meta.env.VITE_API_URL || '';
+
 const API = {
-  USERS: '/api/users',
-  AUTH_USER_INVITES_VALIDATION: '/api/auth/user-invites/validation',
-  AUTH_USERS_LOOKUP: '/api/auth/users/lookup',
+  USERS: `${API_BASE_URL}/api/users`,
+  AUTH_USER_INVITES_VALIDATION: `${API_BASE_URL}/api/auth/user-invites/validation`,
+  AUTH_USERS_LOOKUP: `${API_BASE_URL}/api/auth/users/lookup`,
 };
 
-export const { USERS, AUTH_USER_INVITES_VALIDATION, AUTH_USERS_LOOKUP } = API; 
+export const { USERS, AUTH_USER_INVITES_VALIDATION, AUTH_USERS_LOOKUP } = API;
